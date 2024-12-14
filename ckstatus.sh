90cabf1aaed1496aff155d7c5dd929f2c7aa343a
Daniel R <dantavares@gmail.com>
2024-12-14 14:34:44 -0300

    upt

--- ckstatus.sh
+++ ckstatus.sh
@@ -3,3 +3,4 @@
 cd /var/www/html/status; python3 uptscripts/hosp_upas_hortolandia.py
 cd /var/www/html/status; python3 uptscripts/esc_aracoiaba.py
 cd /var/www/html/status; python3 uptscripts/esc_itapecerica.py
+cd /var/www/html/status; python3 uptscripts/cmv_barueri.py
